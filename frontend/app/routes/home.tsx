ff4a205cc11f0730a3444767e258581c13f66fea
Al <cangzan@gmail.com>
2024-12-26 13:12:20 +0800

    feat: improve

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,4 +1,11 @@
 import { redirect } from 'react-router';
+import ky from 'ky';
+
+export type IUser = {
+  name: string;
+  email: string;
+  pid: string;
+}
 
 export async function loader({ request }: { request: Request }) {
   try {
@@ -10,7 +17,7 @@ export async function loader({ request }: { request: Request }) {
         },
       });
     }
-    const user = await fetch(`${process.env.API_HOST}/api/auth/current`, {
+    const user: IUser = await ky(`${process.env.API_HOST}/api/auth/current`, {
       headers: {
         Cookie: request.headers.get('Cookie') || '',
       },
@@ -27,7 +34,7 @@ export function meta() {
   ];
 }
 
-export default function Home({ loaderData }) {
+export default function Home({ loaderData }: { loaderData: { user: IUser | null } }) {
   return (
     <div>
       User:
