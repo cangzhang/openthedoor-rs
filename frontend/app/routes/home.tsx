ce464b6a0dff614541f1a8ffa710ba250c895ec0
Al <cangzan@gmail.com>
2024-12-26 12:02:46 +0800

    feat: setup ui

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,13 +1,26 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+
+export async function loader({ request }: { request: Request }) {
+  const user = await fetch(`${process.env.API_HOST}/api/auth/current`, {
+    headers: {
+      Cookie: request.headers.get('Cookie') || '',
+    },
+  }).then((r) => r.json());
+  return { user };
+}
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function Home({ loaderData }) {
+  return (
+    <div>
+      User:
+      <pre>{JSON.stringify(loaderData.user, null, 2)}</pre>
+    </div>
+  );
 }
